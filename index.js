6664786c8d90d3260cc8d59444e06b11ce2ccb20
Tracie T <traciecodes@gmail.com>
2019-10-28 18:51:06 -0500

    Started building the home page with pug template

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const path = require('path');
+
+// imports express module
 const express = require('express');
 
+// executes express function and stores it in app variable
 const app = express();
 const port = process.env.PORT || '7500';
 
+// Connects the Pug templates with the controllers:
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// handles GET requests made to the root path and, if the req is successful, renders our pug template for the home page
 app.get('/', (req, res) => {
-	res.status(200).send('WHATABYTE: Food for Devs');
+	res.render('index', { title: 'Home' });
 });
 
 app.listen(port, () => {
